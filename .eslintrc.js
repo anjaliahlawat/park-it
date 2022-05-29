97ef3fb703ee833f15b444dea9122f8b25765f91
anjaliahlawat16 <anjali@primesellerhub.com>
2022-05-29 23:06:52 +0200

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 9,
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts", ".d.ts"],
+      },
+    },
+  },
+  rules: {
+    "no-underscore-dangle": OFF,
+    "import/extensions": [OFF, "never"],
+    "prettier/prettier": ERROR,
+    "max-len": [WARNING, { code: 100 }],
+    "no-console": WARNING,
+    "no-await-in-loop": OFF,
+    "class-methods-use-this": OFF,
+    "no-unused-vars": WARNING,
+  },
+};
